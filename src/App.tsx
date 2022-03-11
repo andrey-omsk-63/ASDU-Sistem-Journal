9c66b8ba0477780d70c4ef540aa7c2e6f0b94c5d
unknown <andrey_omsk@mail.ru>
2022-03-11 13:26:34 +0600

    '1103day'

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,32 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import TabContext from '@mui/lab/TabContext';
 import TabPanel from '@mui/lab/TabPanel';
-//import Modal from '@mui/material/Modal';
+import Modal from '@mui/material/Modal';
 
-//import axios from 'axios';
+import axios from 'axios';
+
 //import Header from './components/Header/Header';
-import Management from './components/Management/Management';
-import Points from './components/Points/Points';
-import Statistics from './components/Statistics/Statistics';
+//import Management from './components/Management/Management';
+//import Points from './components/Points/Points';
+//import Statistics from './components/Statistics/Statistics';
 
-import { Tflight } from './interfaceMNG.d';
+//import { Tflight } from './interfaceMNG.d';
 
-import { XctrlInfo } from './interfaceGl.d';
+//import { XctrlInfo } from './interfaceGl.d';
 
-import { Statistic } from './interfaceStat.d';
+//import { Statistic } from './interfaceStat.d';
 
-let flagWS = true;
-let WS: any = null;
+export interface InpDate {
+  fileNames: string[];
+  message: string;
+}
+
 
 const App = () => {
   const styleApp01 = {
     fontSize: 14,
     marginRight: 1,
-    width: '12%',
+    width: '21%',
     maxHeight: '21px',
     minHeight: '21px',
     backgroundColor: '#F1F3F4',
@@ -78,142 +82,122 @@ const App = () => {
     textTransform: 'unset !important',
   };
 
-  const handleClose = () => {
-    window.close();
+  const styleModalMenu = {
+    fontSize: 13.9,
+    maxHeight: '20px',
+    minHeight: '20px',
+    backgroundColor: '#F1F3F4',
+    color: 'black',
+    marginRight: 1,
+    textTransform: 'unset !important',
   };
 
-  const EndSeans = () => {
-    let soob = 'В Арм-е Технолога системы работает пользователь ' + bsLogin;
+  const styleModal = {
+    position: 'relative',
+    bottom: '-45vh',
+    marginLeft: '60vh',
+    transform: 'translate(-50%, -50%)',
+    width: 200,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 1,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const [crossData, setCrossData] = React.useState(0);
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = (numer: number) => {
+    if (numer !== 777) {
+      setCrossData(numer)
+      setValue('1')
+    }
+    setOpen(false);
+  };
+
+  const SpisData = () => {
+    let resStr = [];
+    for (let i = 0; i < points.length; i++) {
+      resStr.push(
+        <Button key={i} sx={styleModalMenu} variant="contained" onClick={() => handleClose(i)}>
+          <b>
+            {points[i]}
+          </b>
+        </Button>,
+      );
+    }
+    resStr.push(
+      <Button key={777} sx={styleModalMenu} variant="contained" onClick={() => handleClose(777)}>
+        <b>
+          Выход
+        </b>
+      </Button>,
+    );
+    return resStr;
+  };
+
+  const ChoiceData = () => {
     return (
       <>
-        {bsLogin !== '' && (
-          <>
-            <Box sx={styleMod}>
-              <Box sx={{ textAlign: 'center', fontSize: 16, color: 'red' }}>
-                <b>{soob}</b>
-              </Box>
-              <Box sx={{ color: 'background.paper' }}>Pusto</Box>
-              <Box sx={{ textAlign: 'center' }}>
-                <Button sx={styleBatMenu} variant="contained" onClick={handleClose}>
-                  <b>Выход</b>
-                </Button>
-              </Box>
-            </Box>
-          </>
-        )}
+        <Button sx={styleApp01} variant="contained" onClick={handleOpen}>
+          <b>Выберите дату</b>
+        </Button>
+        <Modal
+          open={open}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box sx={styleModal}>
+            <Stack direction="column">{SpisData()}</Stack>
+          </Box>
+        </Modal>
       </>
-    );
-  };
-
-  const [pointsXctrl, setPointsXctrl] = React.useState<Array<XctrlInfo>>([]);
-  const [isOpenInf, setIsOpenInf] = React.useState(false);
-  const [pointsTfl, setPointsTfl] = React.useState<Array<Tflight>>([]);
-  const [isOpenDev, setIsOpenDev] = React.useState(false);
-  const [pointsSt, setPointsSt] = React.useState<Array<Statistic>>([]);
-  const [isOpenSt, setIsOpenSt] = React.useState(false);
-  const [bsLogin, setBsLogin] = React.useState('');
-
-  const host =
-    'wss://' + window.location.host + window.location.pathname + 'W' + window.location.search;
-  // let WS: React.MutableRefObject<WebSocket> = {};
-  //const WS: any = React.useRef(new WebSocket('wss://ws.kraken.com/'));
-  //const WS = React.useRef(new WebSocket('wss://ws.kraken.com/'));
-  if (flagWS) {
-    WS = new WebSocket(host);
-    flagWS = false;
+    )
   }
 
+  // const Close = () => {
+  //   return <h1>
+  //     Выход
+  //     //{window.close()}
+  //   </h1>
+
+  // };
+
+  const [points, setPoints] = React.useState<Array<InpDate>>([]);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const ipAdress: string = 'http://localhost:3000/otladkaGlob.json';
+
   React.useEffect(() => {
-    WS.onopen = function (event: any) {
-      console.log('WS.current.onopen:', event);
-    };
-
-    WS.onclose = function (event: any) {
-      console.log('WS.current.onclose:', event);
-    };
-
-    WS.onerror = function (event: any) {
-      console.log('WS.current.onerror:', event);
-    };
-
-    WS.onmessage = function (event: any) {
-      let allData = JSON.parse(event.data);
-      let data = allData.data;
-      switch (allData.type) {
-        case 'getDevices':
-          setPointsTfl(data.tflight ?? []);
-          setIsOpenDev(true);
-          break;
-        case 'xctrlInfo':
-          //console.log('data_xctrlInfo:', data);
-          setPointsXctrl(data.xctrlInfo ?? []);
-          setIsOpenInf(true);
-          break;
-        case 'getStatistics':
-          setPointsSt(data.statistics ?? []);
-          setIsOpenSt(true);
-          break;
-        case 'busy':
-          setBsLogin(data.login);
-          break;
-        default:
-          console.log('data_default:', data);
-      }
-    };
-  }, []);
+    axios.get(ipAdress).then(({ data }) => {
+      setPoints(data.fileNames);
+      setIsOpen(true);
+    });
+  }, [ipAdress]);
 
   const [value, setValue] = React.useState('1');
 
   return (
     <>
-      <EndSeans />
+
       <Box sx={{ width: '98.8%', typography: 'body2' }}>
         <TabContext value={value}>
           <Box sx={{ marginLeft: 0.5, backgroundColor: '#F1F5FB' }}>
             <Stack direction="row">
-              {bsLogin === '' && (
-                <Button sx={styleApp01} variant="contained" onClick={() => setValue('1')}>
-                  <b>Управление</b>
-                </Button>
-              )}
-              {bsLogin === '' && (
-                <Button sx={styleApp02} variant="contained" onClick={() => setValue('2')}>
-                  <b>Характерные точки</b>
-                </Button>
-              )}
-              {bsLogin === '' && (
-                <Button sx={styleApp01} variant="contained" onClick={() => setValue('3')}>
-                  <b>Статистика</b>
-                </Button>
-              )}
-              {/* {bsLogin === '' && (
-                <Button sx={styleAppExit} variant="contained" onClick={handleClose}>
-                  <b>Выход</b>
-                </Button>
-              )} */}
-              {/* <Header /> */}
+              <ChoiceData />
+              <Button sx={styleApp01} variant="contained" onClick={() => setValue('2')}>
+                <b>Выход</b>
+              </Button>
             </Stack>
           </Box>
           <TabPanel value="1">
-            {WS !== null && (
-              <>
-                <Management open={isOpenDev} ws={WS} points={pointsTfl} xctrll={pointsXctrl} />
-              </>
-            )}
+            <h1> {points[crossData]} </h1>
           </TabPanel>
           <TabPanel value="2">
-            {WS !== null && (
-              <>
-                <Points open={isOpenInf} ws={WS} xctrll={pointsXctrl} />
-              </>
-            )}
-          </TabPanel>
-          <TabPanel value="3">
-            {WS !== null && (
-              <>
-                <Statistics open={isOpenSt} ws={WS} points={pointsSt} />
-              </>
-            )}
+            <h1>
+              Выход
+            </h1>
           </TabPanel>
         </TabContext>
       </Box>
@@ -222,8 +206,3 @@ const App = () => {
 };
 
 export default App;
-
-//const [points, setPoints] = React.useState<XctrlInfo>({} as XctrlInfo);
-
-// const [points, setPoints] = React.useState<Array<XctrlInfo>>([]);
-// const [isOpen, setIsOpen] = React.useState(false);
